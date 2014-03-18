d17550ad348d27c5b7f15c94d2d8f2efc15d3358
Matt Chadburn <matt.chadburn@guardian.co.uk>
2014-03-18 10:12:07 +0000

    Support filter()'ing over options

--- test/option.js
+++ test/option.js
@@ -4,7 +4,12 @@ var assert = require("assert")
   , Option = require('../lib/option.js').Option;
 
 describe('Option', function(){
-    
+
+    var None = false;
+    var transform = function (v) {
+        return v.replace("f", "b") 
+    }
+
     it('should be defined', function(){
         (Option).should.exist;
     })
@@ -29,19 +34,40 @@ describe('Option', function(){
         (o.get()).should.be.None;
     })
    
-    // collections
+    // Collections: Map
     
     it('should be able to map over the Option', function(){
         var o = new Option("foo");
-        (o.map(function (v) {
-            return v.replace("f", "b") 
-        }).get()).should.be.equal("boo");
+        (o.map(transform).get()).should.be.equal("boo");
     })
     
     it('should not be able to map an empty option', function(){
         var o = new Option();
-        (o.map(function (v) { }).get()).should.be.None;
-        (o.map(function (v) { }).getOrElse("nah")).should.be.equal("nah");
+        (o.map(transform).get()).should.be.None;
+        (o.map(transform).getOrElse("nah")).should.be.equal("nah");
+    })
+
+    // Collections: Filter 
+    
+    it('should be able to filter over the Option', function(){
+        var o = new Option("long string is long");
+        (o.filter(function (v) {
+            return (v.length > 5)
+        }).get()).should.be.equal("long string is long");
+    })
+    
+    it('should be able to filter over an Option to produce None', function(){
+        var o = new Option("a");
+        (o.filter(function (v) {
+            return v.length > 100 
+        }).get()).should.be.None;
+    })
+    
+    it('should be able to filter over an empty Option', function(){
+        var o = new Option();
+        (o.filter(function (v) {
+            return v.length > 100 
+        }).get()).should.be.None;
     })
     
 })
